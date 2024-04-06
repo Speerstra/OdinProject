eb7edc5b8d633bb8fe46e9ad0cd6fe9603546e79
Speerstra <sietskespeerstra@gmail.com>
2024-04-06 12:12:25 -0600

    added transparent background to logo text

--- 5_sign_up_form/script.js
+++ 5_sign_up_form/script.js
@@ -2,22 +2,31 @@
 
 function validate_password() {
         let password = document.getElementById('password');
-        let confirmPassword = document.getElementById('confirm_password');
+        let confirmPassword = document.getElementById('confirm-password');
         let passwordMessage = document.getElementById('password-message');
         let createButton = document.getElementById('create');
-        if (password.value == confirmPassword.value) {
-                confirmPassword.style.border = '';
-                confirmPassword.style.borderColor = '';
-                passwordMessage.innerHTML = '';
-                createButton.disabled = false;
-                createButton.style.opacity = 1;
-
-        } else {
-                confirmPassword.style.border = '2px solid';
-                confirmPassword.style.borderColor = 'red';
+        if (password.value != confirmPassword.value) { 
+                
                 passwordMessage.innerHTML = 'Passwords do not match';
-                passwordMessage.style.color = 'red';
-                createButton.disabled = true;
-                createButton.style.opacity = 0.4;
-      }
+                confirmPassword.setAttribute('invalid', true)
+                confirmPassword.focus(); 
+                return false; 
+        }
+//         if (password.value == confirmPassword.value) {
+//                 confirmPassword.style.border = '';
+//                 confirmPassword.style.borderColor = '';
+//                 passwordMessage.innerHTML = '';
+//                 createButton.disabled = false;
+//                 createButton.style.opacity = 1;
+
+//         } else {
+//                 // confirmPassword.style.border = '2px solid';
+//                 // confirmPassword.style.borderColor = 'red';
+//                 confirmPassword.setAttribute('invalid', true)
+//                 passwordMessage.innerHTML = 'Passwords do not match';
+//                 confirmPassword.focus();
+//                 passwordMessage.style.color = 'red';
+//                 createButton.disabled = true;
+//                 createButton.style.opacity = 0.4;
+//       }
 }
