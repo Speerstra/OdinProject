fe251d90dd862322bbbc7cc0a4422757cdf92787
Speerstra <sietskespeerstra@gmail.com>
2024-04-06 12:48:24 -0600

    moved validation to js file

--- 5_sign_up_form/script.js
+++ 5_sign_up_form/script.js
@@ -1,12 +1,10 @@
-// let createButton = document.getElementById('create');
+let password = document.getElementById('password');    
+let passwordMessage = document.getElementById('password-message');
+let confirmPassword = document.getElementById('confirm-password');
 
-// createButton.addEventListener('click', validate_password)
+confirmPassword.addEventListener('onchange', validate_password());
 
 function validate_password() {
-        let password = document.getElementById('password');
-        let confirmPassword = document.getElementById('confirm-password');
-        let passwordMessage = document.getElementById('password-message');
-
         if (password.value != confirmPassword.value) {    
                 passwordMessage.innerHTML = 'Passwords do not match!';
                 confirmPassword.focus(); 
