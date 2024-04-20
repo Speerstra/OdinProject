ec6835ba7205a41fbb1f652ce459e58abccabc80
Speerstra <sietskespeerstra@gmail.com>
2024-04-19 22:14:33 -0600

    display books implemented

--- 7_library/script.js
+++ 7_library/script.js
@@ -1,31 +1,105 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector('.add-button');
-const closeButton = document.querySelector("dialog button");
 
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
+// ****************
+// Query selection
+const dialog = document.querySelector("dialog");
+const addButton = document.querySelector('.add-button');
+const closeButton = document.querySelector("dialog button");
+const readButton = document.querySelector('.button-read-status')
+const addForm = document.querySelector('.form-add-book')
+const inputTitle = document.querySelector('#input-title')
+const inputAuthor = document.querySelector('#input-author')
+const inputPages = document.querySelector('#input-pages')
+const inputReadStatus = document.querySelector('#input-read-status')
+const bookGrid = document.querySelector('.book-grid')
+
+
+// ****************
+// Event listeners
+addButton.addEventListener("click", () => {
+        inputTitle.value = ''
+        inputAuthor.value = ''
+        inputPages.value = ''
+        inputReadStatus.checked = false;
         dialog.showModal();
 });
 
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener('click', () => {
         dialog.close();
 });
 
+addForm.onsubmit = e => {
+        e.preventDefault();
+        addBookToLibrary();
+        displayBooks();
+        dialog.close();
+};
 
-const myLibrary = [];
+readButton.addEventListener('click', () => {
+        
+});
+
+// ****************
+// Objects
+const library = [];
 
 function Book(title, author, pages, is_read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.is_read = is_read;
+};
 
-        this.info = function() {
-                return `${this.name} by ${this.author}, ${this.pages} pages`
+Book.prototype.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages`;
+};
+
+
+// ****************
+// Functions
+function displayBooks() {
+        for (var i=0; i<library.length; i++) { 
+                let book = library[i];
+                const card = document.createElement("div");
+                card.classList.add('card');
+
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add('button-delete');
+                deleteButton.innerHTML = 'X'
+                card.append(deleteButton)
+
+                const cardTitle = document.createElement("div");
+                cardTitle.classList.add('card-title');
+                cardTitle.innerText = book.title
+                card.append(cardTitle)
+
+                const cardAuthor = document.createElement("div");
+                cardAuthor.classList.add('card-author');
+                cardAuthor.innerText = book.author
+                card.append(cardAuthor)
+
+                const cardPages = document.createElement("div");
+                cardPages.classList.add('card-pages');
+                cardPages.innerText = book.pages
+                card.append(cardPages)
+
+                const cardReadStatus = document.createElement("div");
+                cardReadStatus.classList.add('card-read-status');
+                cardReadStatus.innerText = book.is_read
+                card.append(cardReadStatus)
+
+                bookGrid.append(card)
+
         }
 }
 
 function addBookToLibrary() {
-  // do stuff here
-}
+        let newBook = new Book(
+                title=inputTitle.value, 
+                author=inputAuthor.value, 
+                pages=inputPages.value, 
+                is_read=inputReadStatus.value
+                );
+        console.log(newBook.info())
+        library.push(newBook)
+};
+
