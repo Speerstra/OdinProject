1860e1faa31bdd847ba1cedb07ff11f145649451
Speerstra <sietskespeerstra@gmail.com>
2024-06-09 01:56:16 -0600

    footer links

--- 9_restaurant/src/pages/home.js
+++ 9_restaurant/src/pages/home.js
@@ -33,7 +33,7 @@ const loadHome = function(){
         homeDiv.appendChild(navButtonContainer)
 
         const footer = document.createElement("div");
-        footer.innerHTML = 'Images from <a href="https://unsplash.com/">Upsplash<a>'
+        footer.innerHTML = 'Copyright © 2024 <a href="https://github.com/speerstra" target="_blank" class="github-icon">Speerstra  <i class="fab fa-github"></i></a> <br />Images from <a href="https://unsplash.com/">Upsplash<a>'
         footer.id = 'footer'
         homeDiv.appendChild(footer)
 
