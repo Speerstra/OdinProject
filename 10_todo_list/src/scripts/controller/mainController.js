a228943141d3446844a90e010cd1ad5c480b9c3b
Speerstra <sietskespeerstra@gmail.com>
2024-06-15 14:07:32 -0600

    open and close dialog for add form

--- 10_todo_list/src/scripts/controller/mainController.js
+++ 10_todo_list/src/scripts/controller/mainController.js
@@ -2,29 +2,52 @@ export default class MainController {
         constructor(model, view) {
             this.model = model;
             this.view = view;
-    
-            view.getById('addFormSubmitBtn').addEventListener('click', (e) => this.handleAddFormSubmit(e));
+            
+            view.getById('add-task-btn').addEventListener('click', () => this.openAddDialog());
+            view.getById('add-form-submit-btn').addEventListener('click', (e) => this.addTaskFromInput(e));
+            view.getById('add-project-btn').addEventListener('click', (e) => this.openProjectDialog(e));
+        // add project
+        // delete project
+            //  view.getById('addTaskButton').addEventListener('click', dialog.showModal();)
+        //  view.getById('closeAddTaskButton').addEventListener('click', dialog.close() and resetForm();)
+        //  view.getById('addProjectButton').addEventListener('click', dialog.showModal();)
+        //  view.getById('closeAddProjectButton').addEventListener('click', dialog.close() and resetForm();)
+        //  filter displays
+        //  toggle task as complete
+        //  delete task
+        // window.addeventlistener('load', getdatafromstorage)
+
+
         }
         
-        getAddFormInputs() {
-                const taskName = this.view.getTaskFormValue('task-name');
-                const taskProject = this.view.getTaskFormValue('task-project');
-                const taskDueDate = this.view.getTaskFormValue('task-dueDate');
-                const taskIsImportant= this.view.getTaskFormValue('task-isImportant');
-
-                return {taskName, taskProject, taskDueDate, taskIsImportant}
-        }
         
 
-        handleAddFormSubmit = (e) => {
+        openAddDialog = () => {
+                const dialog = document.querySelector("dialog");
+                dialog.showModal();
+        };   
+
+        closeAddDialog = () => {
+                const dialog = document.querySelector("dialog");
+                dialog.close();
+        };  
+
+        addTaskFromInput = (e) => {
             e.preventDefault(); 
 
-            let formInputs = this.getAddFormInputs()
+            let formInputs = this.view.getTaskFormInput();
             let newTask = this.model.createTask(formInputs);
             let newTaskList = this.model.addTaskToList(newTask);
             this.model.saveTaskListToLocalStorage(newTaskList);
             this.view.resetForm('add-task-form')
+            this.closeAddDialog();
 
             console.log('taskList: ', JSON.parse(localStorage.getItem('taskList'))); 
+        //     display tasks()
+        }
+
+        openProjectDialog = (e) => {
+                e.preventDefault(); 
+                console.log('add project')
         }
     }
--- 10_todo_list/src/scripts/view/mainView.js
+++ 10_todo_list/src/scripts/view/mainView.js
@@ -7,8 +7,17 @@ export default class MainView {
                 return document.getElementById(id);
         }
 
-        getTaskFormValue(name) {
+        getTaskFormField(name) {
             return this.getById('add-task-form').elements[name].value;
+        }
+
+        getTaskFormInput() {
+                const taskName = this.getTaskFormField('task-name');
+                const taskProject = this.getTaskFormField('task-project');
+                const taskDueDate = this.getTaskFormField('task-dueDate');
+                const taskIsImportant= this.getTaskFormField('task-isImportant');
+
+                return {taskName, taskProject, taskDueDate, taskIsImportant}
         }
 
         resetForm(formID) {
